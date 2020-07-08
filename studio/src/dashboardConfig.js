24bdc258385993883612df76dda0b6e9078cb51a
hutingerica <hutingstudio@gmail.com>
2020-07-08 14:33:23 +0100

    Enable real-time content preview on development

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -12,16 +12,16 @@ export default {
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
                 {
-                  buildHookId: '',
+                  buildHookId: '5f05c584d936a7587fffdfd0',
                   title: 'Sanity Studio',
-                  name: '',
-                  apiId: ''
+                  name: 'beyond-flavour-cms-studio',
+                  apiId: 'd54d8bac-320a-41c7-add0-637e04185fb4'
                 },
                 {
-                  buildHookId: '',
+                  buildHookId: '5f05c584c927e7601927992b',
                   title: 'Blog Website',
-                  name: '',
-                  apiId: ''
+                  name: 'beyond-flavour-website',
+                  apiId: '4954239e-df42-446e-b75a-ca8e836b25e7'
                 }
               ]
             }
